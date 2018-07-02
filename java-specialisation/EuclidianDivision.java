c330b139fc93a48a52bd78782dd90d1a8c964c59
SLY@237 <ktanyu.kt@gmail.com>
2018-07-01 22:50:24 -0500

    gcd java v0

--- java-specialisation/EuclidianDivision.java
+++ java-specialisation/EuclidianDivision.java
@@ -1,6 +1,35 @@
 
+import java.util.Scanner;
 
-Public class EuclidianDivision {
+public class EuclidianDivision {
 
+public static int euclidDiv(int num1, int num2) {
+    int small, diff, gcd;
+    if ((num1 > num2) && (num1 != num2)) {
+        small = num2;
+        diff = num1 - small;
+        gcd = euclidDiv(diff,small);
+    } else if (num1 == num2) {
+        gcd = small = num2;
+    } else {
+        small = num1;
+        diff = num2 - small;
+        gcd = euclidDiv(diff, small);
+    }
+    
+    return gcd;
+} 
+
+    public static void main(String [] args) {
+    int num1, num2;
+    Scanner scan = new Scanner(System.in);    
+
+    System.out.println("Enter first number: ");
+    num1 = scan.nextInt();
+    System.out.println("Enter Second number: ");
+    num2 = scan.nextInt();
+    System.out.println(euclidDiv(num1, num2));
+    
+    }
 }
 
